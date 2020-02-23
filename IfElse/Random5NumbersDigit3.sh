e6d402b057219cd4b1fd3436254f713de7efbcc4
Prathamaesh <prathmesh6464@gmail.com>
2020-02-24 01:14:32 +0530

    REFACTORED : Intendation changed,varibale name changed

--- IfElse/Random5NumbersDigit3.sh
+++ IfElse/Random5NumbersDigit3.sh
@@ -1,39 +1,44 @@
+#!/bin/bash -x
+#VARIABLE
 randomNum1=$((RANDOM%900+100))
 randomNum2=$((RANDOM%900+100))
 randomNum3=$((RANDOM%900+100))
 randomNum4=$((RANDOM%900+100))
 randomNum5=$((RANDOM%900+100))
-echo "Radom 5 Numbers "
 
-	minValue=$randomNum1
-	maxValue=$randomNum1
-	if [ $randomNum2 -lt $minValue ]
-	then 
-		minValue=$randomNum2
-	elif [ $randomNum3 -lt $minValue ]
-	then
-		minValue=$randomNum3
-	elif [ $randomNum4 -lt $minValue ]
-	then 
-		minValue=$randNum4
-	elif [ $randomNum5 -lt $minValue ]
-	then
-		minValue=$randomNum5
-	fi
+#FINDING MINIMUM VALUE
+minValue=$randomNum1
+maxValue=$randomNum1
+if [ $randomNum2 -lt $minValue ]
+then 
+	minValue=$randomNum2
+elif [ $randomNum3 -lt $minValue ]
+then
+	minValue=$randomNum3
+elif [ $randomNum4 -lt $minValue ]
+then 
+	minValue=$randNum4
+elif [ $randomNum5 -lt $minValue ]
+then
+	minValue=$randomNum5
+fi
 echo $minValue
-	if [ $randomNum2 -gt $maxValue ]
-	then
-		maxValue=$randomNum2
-	elif [ $randomNum3 -gt $maxValue ]
-	then
-		maxValue=$randomNum3
-	elif [ $randomNum4 -gt $maxValue ]
-	then
-		maxValue=$randomNum4
-	elif [ $randomNum5 -gt $maxValue ]
-	then
-		maxValue=$randomNum5
-	fi
+
+
+#FINDIN MAXIMUM VALUE
+if [ $randomNum2 -gt $maxValue ]
+then
+	maxValue=$randomNum2
+elif [ $randomNum3 -gt $maxValue ]
+then
+	maxValue=$randomNum3
+elif [ $randomNum4 -gt $maxValue ]
+then
+	maxValue=$randomNum4
+elif [ $randomNum5 -gt $maxValue ]
+then
+	maxValue=$randomNum5
+fi
 echo $maxValue
 
 
