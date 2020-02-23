4b50673e09b4fa024af35da0f4d4779a648fa89a
Prathamaesh <prathmesh6464@gmail.com>
2020-02-24 01:14:19 +0530

    REFACTORED : Intendation changed,varibale name changed

--- IfElse/LeapYear.sh
+++ IfElse/LeapYear.sh
@@ -1,6 +1,6 @@
-read -p "Enter Year To Check Wheather It is Leap or Not : " year
+#!/bin/bash -x
+read -p "Enter Year To Check Wheather It Is Leap Or Not : " year
 echo "${#year}"
-
 if [[ "${#year}" -eq 4 ]];
 then
 	if [[ $year%4 -eq 0 && $year%100 -ne 0 || $year%400 -eq 0 ]]
