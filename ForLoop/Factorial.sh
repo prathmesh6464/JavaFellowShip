60c6dc8ee8566723c53a46f71b398c055c65920c
Prathamaesh <prathmesh6464@gmail.com>
2020-02-23 23:13:48 +0530

    REFACTORED : Changed Intendation,varibale name

--- ForLoop/Factorial.sh
+++ ForLoop/Factorial.sh
@@ -1,7 +1,14 @@
+#!/bin/bash -x
+
+
+#FIND FACTORIAL NUMBER
 read -p "Enter To Compute Factorial" number
 factorial=1
-for ((i=$number;i>=1;i--))
+for ((iter=$number;iter>=1;iter--))
 do	
-	factorial=$((factorial*i))
+	factorial=$((factorial*iter))
 done
+
+
+#DISPLAY FACTORIAL
 echo $factorial
