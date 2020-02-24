b27a151bffa9f079cf4e1ce63cba904b86595bbe
prathamesh <prathmesh6464@gmail.com>
2020-02-24 18:18:33 +0530

    ADDED : GenerateBirthMonthCount.sh which shows count of birthday in each month

---
+++ DictionaryPrograms/GenerateBirthMonthCount.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -x
+#CONSTANT VARIABLE
+INDIVIDUALS_NUMBER=50
+NUMBER_OF_MONTH=12
+
+
+#VARIABLE
+declare -A birthDateArray
+
+
+#LOGIC TO GENERATE 50 INDIVIDULAS BIRTHDATE
+for((iter=1;iter<=INDIVIDUALS_NUMBER;iter++))
+do
+	randomBirthMonth=$((RANDOM%12+1))
+	birthDateArray[$randomBirthMonth]=$(("${birthDateArray[$randomBirthMonth]}"+1))
+done
+
+
+#PRINTING BIRTHDATE AND NO OF PEOPLE BIRTHDAY
+for((iter=1;iter<=NUMBER_OF_MONTH;iter++))
+do
+	echo "$iter Month have ${birthDateArray[$iter]} Birthdays"
+done
