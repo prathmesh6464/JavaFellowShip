112406bdab2ae784a470793a677275c9947d6c53
Prathamesh Bhiku Jadhav <37609950+prathmesh6464@users.noreply.github.com>
2020-02-24 12:53:10 +0530

    MODIFIED : Changed indentation

--- Function/degreeConversion.sh
+++ Function/degreeConversion.sh
@@ -1,31 +1,34 @@
 #!/bin/bash -x
-echo "Enter Your Choice";
-echo "1-Celcius To Farheneit";
-echo "2-Farheneit To Celcius";
+echo "Enter Your Choice"
+echo "1-Celcius To Farheneit"
+echo "2-Farheneit To Celcius"
 read choice
 
 
 celciusToFarheneit(){
 	celcius=$1
-	ferheneit=`echo "$celcius*9/5+32" | bc -l`;
-        echo "Value In Farheneit Is : " $ferheneit;
+	ferheneit=`echo "$celcius*9/5+32" | bc -l`
+        echo "Value In Farheneit Is : " $ferheneit
 }
 
 
 farheneitToCelcius(){ 
 	ferheneit=$1
-	celcius=`echo "$farheneit-32*5/9" | bc -l`;
-	echo "Value In Celcius Is : " $celcius;
+	celcius=`echo "$farheneit-32*5/9" | bc -l`
+	echo "Value In Celcius Is : " $celcius
 }
 
 
 case $choice in
-        1)echo "Enter The Value In Celcius";
-        read celcius
-        celciusToFarheneit $celciuS;;
-        2)echo "Enter The value In farheneit";
-        read farheneit
-        farheneitToCelcius $farheneit;;
-        *) echo "Invalid Input";;
+        1)
+		echo "Enter The Value In Celcius"
+       		read celcius
+        	celciusToFarheneit $celciuS;;
+        2)
+		echo "Enter The value In farheneit"
+        	read farheneit
+        	farheneitToCelcius $farheneit;;
+        *) 
+		echo "Invalid Input";;
 esac
 
