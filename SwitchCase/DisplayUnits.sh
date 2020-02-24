4c79b8e833113f976e8e762e1e1670fa43a69694
prathamesh <prathmesh6464@gmail.com>
2020-02-24 11:58:21 +0530

    MODIFIED : Variable name changed

--- SwitchCase/DisplayUnits.sh
+++ SwitchCase/DisplayUnits.sh
@@ -2,33 +2,33 @@
 read -p "Enter Number To Convert into Units " number
 case "${#number}" in 
 	1)
-		echo $number in unit 1 place;;
+		echo $number "in unit 1 place";;
 	2)
 		num=$(($number%10))
-		echo $num in unit 1 place
+		echo $num "in unit 1 place";
 		number=$(($number/10))
-		echo $number in unit 10 place;;
+		echo $number "in unit 10 place";;
 	3)
 		num=$(($number%10))
-		echo $num in unit 1 place
+		echo $num "in unit 1 place";
 		number=$(($number/10))
 		num=$(($number%10))
-		echo $num in unit 10 place;
+		echo $num "in unit 10 place";
 		number=$(($number/10))
-		echo $number in Unit 100 place;;
+		echo $number "in Unit 100 place";;
 
 	4)
 		num=$(($number%10))
-		echo $num in unit 1 place
+		echo $num "in unit 1 place";
 		number=$(($number/10))
 		num=$(($number%10))
-		echo $num in unit 10 place
+		echo $num "in unit 10 place";
 		number=$(($number/10))
 		num=$(($number%10))
-		echo $num in unit 100 place
+		echo $num "in unit 100 place";
 		number=$(($number/10))
-		echo $number in unit 1000 place ;;
+		echo $number "in unit 1000 place";;
 	*)
-		printf "Please Enter Number Between 0 to 1000 \n"
+		printf "Please Enter Number Between 0 to 1000 \n";;
 
 esac
