6b8c9b04fe326ddd8f57621211c7f35fa708467c
prathamesh <prathmesh6464@gmail.com>
2020-02-24 11:47:14 +0530

    MODIFIED : Variable name changed

--- RandomNumberProgram/Generate5RandomNumber2digit.sh
+++ RandomNumberProgram/Generate5RandomNumber2digit.sh
@@ -1,14 +1,13 @@
 #!/bin/bash -x
-value1=$((RANDOM%99))
-value2=$((RANDOM%99))
-value3=$((RANDOM%99))
-value4=$((RANDOM%99))
-value5=$((RANDOM%99))
-if [[ "${#value1}" == 2 && "${#value2}" == 2 && "${#value3}" == 2 && "${#value4}" == 2 && "${#value5}" == 2 ]]
-then 
-	echo "sum  of " $value1 , $value2 , $value3 , $value4 , $value5 "is : " $(($value1+$value2+$value3+$value4+$value5))
-	echo "average of " $value1 , $value2 , $value3 , $value4 , $value5 "is : " $(($(($value1+$value2+$value3+$value4+$value5))/5))
-else
-	echo "2 No digit has not been generated please run again"
+#VARIABLE
+randomValue1=$((RANDOM%90+10))
+randomValue2=$((RANDOM%90+10))
+randomValue3=$((RANDOM%90+10))
+randomValue4=$((RANDOM%90+10))
+randomValue5=$((RANDOM%90+10))
 
-fi
+
+#SUM AND AVERAGE OF RANDOM VARIABLE
+echo "sum  of "  $randomValue1 , $randomValue2 , $randomValue3 , $randomValue4 , $randomValue5 "is : " $(($randomValue1+$randomValue2+$randomValue3+$randomValue4+$randomValue5))
+echo "average of "  $randomValue1 , $randomValue2 , $randomValue3 , $randomValue4 , $randomValue5 "is : " $(($(($randomValue1+$randomValue2+$randomValue3+$randomValue4+$randomValue5))/5))
+
